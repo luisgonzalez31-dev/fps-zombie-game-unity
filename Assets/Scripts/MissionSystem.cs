a78d0e8c2e9438aaef060affaa2cae293444eb99
Luis Gonzalez <gatoluiso2010@gmail.com>
2026-03-26 12:54:36 -0600

    Implement MissionManager for zombie kill tracking

--- Assets/Scripts/MissionSystem.cs
+++ Assets/Scripts/MissionSystem.cs
@@ -1,1 +1,53 @@
+using UnityEngine;
+using TMPro;
 
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager instance;
+
+    public int zombiesToKill = 30;
+    private int zombiesKilled = 0;
+    private bool missionComplete = false;
+
+    public TextMeshProUGUI missionText;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        UpdateMissionUI();
+    }
+
+    public void ZombieKilled()
+    {
+        zombiesKilled++;
+
+        UpdateMissionUI();
+
+        if (zombiesKilled >= zombiesToKill)
+        {
+            MissionComplete();
+        }
+    }
+
+    void UpdateMissionUI()
+    {
+        missionText.text = "Kill Zombies: " + zombiesKilled + " / " + zombiesToKill;
+    }
+
+    void MissionComplete()
+    {
+        missionComplete = true;
+
+        missionText.text = "MISSION COMPLETE! GO TO EXTRACTION";
+
+        GameManager.instance.AddMoney(100);
+    }
+    public bool IsMissionComplete()
+    {
+        return missionComplete;
+    }
+}
