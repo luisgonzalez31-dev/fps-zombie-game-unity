137c566886dcd6ef8998fd1936c047fd33759fc3
Luis Gonzalez <gatoluiso2010@gmail.com>
2026-03-26 12:59:44 -0600

    Implement NPCManager for managing NPC rescue logic

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -1,1 +1,29 @@
+using UnityEngine;
 
+public class NPCManager : MonoBehaviour
+{
+    public static NPCManager instance;
+
+    public bool mechanicRescued = false;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        SaveSystem.instance.LoadGame();
+    }
+    public void RescueNPC(string npcName)
+    {
+        if (npcName == "Mechanic")
+        {
+            mechanicRescued = true;
+            SaveSystem.instance.SaveGame();
+        }
+    }
+}
