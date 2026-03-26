3768c03df94543e836209de5c14f5b0f0e4afd25
Luis Gonzalez <gatoluiso2010@gmail.com>
2026-03-26 12:42:50 -0600

    Implement basic gun shooting mechanics

--- Assets/Scripts/GunSystem.cs
+++ Assets/Scripts/GunSystem.cs
@@ -1,1 +1,34 @@
+using UnityEngine;
 
+public class GunSystem : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCamera;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("Hit: " + hit.transform.name);
+
+            ZombieHealth zombie = hit.transform.GetComponent<ZombieHealth>();
+
+            if (zombie != null)
+            {
+                zombie.TakeDamage(damage);
+            }
+        }
+    }
+}
